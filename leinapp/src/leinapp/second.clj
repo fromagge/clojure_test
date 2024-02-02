72290ff57dc5f751451225780c1af0377c418c55
Gabriel Garcia <ygabo@pm.me>
2024-02-01 21:03:39 -0500

    refactor second task

--- leinapp/src/leinapp/second.clj
+++ leinapp/src/leinapp/second.clj
@@ -4,55 +4,83 @@
             [clojure.set :as st]
             [leinapp.invoice-spec :as spec])
   (:import [java.text SimpleDateFormat]))
+(use 'clojure.walk)
 
 (defn read_file [f] (json/read-str (slurp f)))
 
-(defn str->date [str]
+(defn val->ns
+  "Add namespace to map in clojure"
+  [ns data]
+  (->> data
+       (map (fn [[k v]] [(keyword (name ns) (name k)) v]))
+       (into {})))
+
+
+(defn str->date
+  "Turn string to date"
+  [str]
   (let [date-format (SimpleDateFormat. "dd/MM/yyyy")
         parsed (.parse date-format str)]
     parsed))
 
-(defn str->ns-keyword
-  "Turns all keys in the map into a namespace keyword"
-  [_map ns]
-  (cond
-    (map? _map) (into {}
-                      (for [[k v] _map]
-                        [(keyword ns k) (str->ns-keyword v nil)]))
-    (vector? _map) (vector (map (if (nil? ns) #(str->ns-keyword % nil) #(str->ns-keyword % ns)) _map))
-    :else _map))
+(defn update-key
+  "Replacement of update func to work with thread-last operator"
+  [k f v]
+  (update v k f))
 
 (defn format_customer
+  "Renaming keys of customer"
   [customer]
-  (st/rename-keys customer {"company_name" :customer/name, "email" :customer/email}))
+  (st/rename-keys customer {:company_name :customer/name, :email :customer/email}))
 
-(defn format_tax [t]
-  (identity {:tax/category (keyword (clojure.string/lower-case (t :tax_category))) :tax/rate (double (t :tax_rate))}))
+(defn format_tax [tax]
+    (->> tax
+         (#(st/rename-keys % {:tax_rate :rate, :tax_category :category}))
+         (val->ns :tax)
+         (update-key :tax/category (constantly :iva))
+         (update-key :tax/rate double)))
+
+(defn format_taxes [taxes]
+  (vec (for [tax taxes]
+          (format_tax tax))))
 
 
-(defn format_taxes [t]
-  (let [taxes (first t)]
-    (vec (map format_tax taxes))))
+(defn format_invoice_item
+  [ii]
+  (->> ii
+       (val->ns :invoice-item)
+       (update-key :invoice-item/taxes format_taxes)))
 
-
-(defn format_items [items]
-  (let [formatted (map #(str->ns-keyword % "invoice-item") items)]
-    (vec (map #(assoc % :invoice-item/taxes (format_taxes (:invoice-item/taxes %))) formatted))))
-
+(defn format_invoice_items [items]
+  (vec (for [ii items]
+         (->> ii
+              (format_invoice_item)))))
 
 (defn parse
   "Parse invoice json file to correct format"
   [filename]
-  (let [raw ((read_file filename) "invoice")
-        w_date (assoc {} :invoice/issue-date (str->date (raw "issue_date")))
-        w_customer (assoc w_date :invoice/customer (format_customer (raw "customer")))
-        w_items (assoc w_customer :invoice/items (format_items (raw "items")))]
-    (identity w_items)))
+  (let [file (read_file filename)
+        m (clojure.walk/keywordize-keys file)]
+    (->> m
+         (:invoice)
+         (val->ns :invoice)
+         (update-key :invoice/customer format_customer)
+         (update-key :invoice/issue_date str->date)
+         (#(st/rename-keys % {:invoice/issue_date :invoice/issue-date})) ;; easiest way I found, my bad
+         (update-key :invoice/items format_invoice_items))))
+
 
 
 (def parse_data (parse "invoice.json"))
-(println parse_data)
-(s/valid? ::spec/invoice parse_data) ;; => true ;)
+(identity parse_data)
+(s/valid? ::spec/invoice parse_data) ;
+; => true ;)
+
+
+
+
+
+
 
 
 
